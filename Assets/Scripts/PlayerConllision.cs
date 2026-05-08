1e96bd40208d34d67f7a6d40827f2b673049d5c2
Phat-1233 <nnguyenthanhdat9@gmail.com>
2026-05-09 01:06:27 +0700

    coin và point ui

---
+++ Assets/Scripts/PlayerConllision.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerConllision : MonoBehaviour
+{
+    private GameManager gameManager;
+    private void Awake()
+    {
+        gameManager = FindAnyObjectByType <GameManager>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Coin"))
+        {
+            Destroy(collision.gameObject);
+            gameManager.AddScore(1);
+        }
+    }
+}
